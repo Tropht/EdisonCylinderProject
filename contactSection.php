ec343d4998382c1552e426ffed0a0e640f76722e
pandagirlxx <mengxili@Mengxis-MBP.ad.uvu.edu>
2017-03-01 19:32:36 -0700

    Merge branch 'master' into pandagirlxx-patch-1
    # Conflicts:
    #	index.html

---
+++ contactSection.php
@@ -0,0 +1,17 @@
+<header>
+  <ul>
+    <li>
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+    </li>
+  </ul>
+</header>
+<main style="background-color: green;">
+
+    <!-- Place your HTML here -->
+    <div>
+    	<form>	
+    		<input>
+    	</form>	
+    </div>
+
+</main>
